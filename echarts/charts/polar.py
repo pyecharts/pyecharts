c85e220311cd6fad2df1c858a498d09359f2f03a
chenjiandongx <chenjiandongx@qq.com>
2017-06-26 00:01:29 +0800

    daily update

---
+++ echarts/charts/polar.py
@@ -0,0 +1,12 @@
+from echarts.base import Base
+
+class Polar(Base):
+
+    def __init__(self, title="", subtitle="", **kwargs):
+        super().__init__(title, subtitle, **kwargs)
+
+    # def add(self, *args, **kwargs):
+    #     self.__add(*args, **kwargs)
+
+    def __add(self):
+        pass
