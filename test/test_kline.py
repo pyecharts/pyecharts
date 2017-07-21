6ca3f0325c6664311e8afd9119f50cf438b7dfb7
chenjiandongx <chenjiandongx@qq.com>
2017-07-22 01:01:17 +0800

    增加k线图

--- test/test_kline.py
+++ test/test_kline.py
@@ -1,41 +1,11 @@
 #!/usr/bin/env python
 #coding=utf-8
 
-from pyecharts import Bar, Line, Scatter, EffectScatter, Kline
+from pyecharts import Kline
 
-def test_custom():
+def test_kline():
 
-    # custom_0
-    attr = ['A', 'B', 'C', 'D', 'E', 'F']
-    v1 = [10, 20, 30, 40, 50, 60]
-    v2 = [15, 25, 35, 45, 55, 65]
-    v3 = [38, 28, 58, 48, 78, 68]
-    bar = Bar("Line - Bar 示例")
-    bar.add("bar", attr, v1)
-    line = Line()
-    line.add("line", v2, v3)
-    bar.custom(line.get_series())
-    bar.show_config()
-    bar.render()
-
-    # custom_1
-    v1 = [10, 20, 30, 40, 50, 60]
-    v2 = [30, 30, 30, 30, 30, 30]
-    v3 = [50, 50, 50, 50, 50, 50]
-    v4 = [10, 10, 10, 10, 10, 10]
-    es = EffectScatter("Scatter - EffectScatter 示例")
-    es.add("es", v1, v2)
-    scatter = Scatter()
-    scatter.add("scatter", v1, v3)
-    es.custom(scatter.get_series())
-    es_1 = EffectScatter()
-    es_1.add("es_1", v1, v4, symbol='pin', effect_scale=5)
-    es.custom(es_1.get_series())
-    es.show_config()
-    es.render()
-
-    # custom_2
-    import random
+    # kline_0
     v1 = [[2320.26, 2320.26, 2287.3, 2362.94],
           [2300, 2291.3, 2288.26, 2308.38],
           [2295.35, 2346.5, 2295.35, 2345.92],
@@ -67,14 +37,13 @@ def test_custom():
           [2309.16, 2286.6, 2264.83, 2333.29],
           [2282.17, 2263.97, 2253.25, 2286.33],
           [2255.77, 2270.28, 2253.31, 2276.22]]
-    attr = ["2017/7/{}".format(i + 1) for i in range(31)]
-    kline = Kline("Kline - Line 示例")
-    kline.add("日K", attr, v1)
-    line_1 = Line()
-    line_1.add("line-1", attr, [random.randint(2400, 2500) for _ in range(31)])
-    line_2 = Line()
-    line_2.add("line-2", attr, [random.randint(2400, 2500) for _ in range(31)])
-    kline.custom(line_1.get_series())
-    kline.custom(line_2.get_series())
+    kline = Kline("K 线图示例")
+    kline.add("日K", ["2017/7/{}".format(i + 1) for i in range(31)], v1)
+    kline.show_config()
+    kline.render()
+
+    # kline_1
+    kline = Kline("K 线图示例")
+    kline.add("日K", ["2017/7/{}".format(i + 1) for i in range(31)], v1, mark_point=["max"], is_datazoom_show=True)
     kline.show_config()
     kline.render()
