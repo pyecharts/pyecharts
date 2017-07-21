da2b4a6521130e24c22caf4165750a168d70d19f
chenjiandongx <chenjiandongx@qq.com>
2017-07-21 18:38:34 +0800

    修改 temple, 增加 custom 示例

--- test/test_custom.py
+++ test/test_custom.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #coding=utf-8
 
-from pyecharts import Bar, Line
+from pyecharts import Bar, Line, Scatter, EffectScatter
 
 def test_custom():
 
@@ -17,3 +17,19 @@ def test_custom():
     bar.custom(line.get_series())
     bar.show_config()
     bar.render()
+
+    # custom_1
+    v1 = [10, 20, 30, 40, 50, 60]
+    v2 = [30, 30, 30, 30, 30, 30]
+    v3 = [50, 50, 50, 50, 50, 50]
+    v4 = [10, 10, 10, 10, 10, 10]
+    es = EffectScatter("Scatter - EffectScatter 示例")
+    es.add("es", v1, v2)
+    scatter = Scatter()
+    scatter.add("scatter", v1, v3)
+    es.custom(scatter.get_series())
+    es_1 = EffectScatter()
+    es_1.add("es_1", v1, v4, symbol='pin', effect_scale=5)
+    es.custom(es_1.get_series())
+    es.show_config()
+    es.render()
