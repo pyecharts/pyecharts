fc2fcbebc91e5c3c73a4cec5ac63a3ead7539820
chenjiandongx <chenjiandongx@qq.com>
2017-07-24 13:32:43 +0800

    增加热力图

--- pyecharts/base.py
+++ pyecharts/base.py
@@ -29,7 +29,7 @@ class Base(object):
         :param height:
             画布高度
         :param title_pos:
-            标题位置，有 auto，left，right，center 可选
+            标题位置，有'auto', 'left', 'right', 'center'可选
         :param title_color:
             主标题文本颜色
         :param subtitle_color:
@@ -44,7 +44,10 @@ class Base(object):
         self._option = {}
         self._width, self._height = width, height
         self._colorlst = ['#c23531', '#2f4554', '#61a0a8', '#d48265', '#749f83',
-                          '#ca8622', '#bda29a', '#6e7074', '#546570', '#c4ccd3']
+                          '#ca8622', '#bda29a', '#6e7074', '#546570', '#c4ccd3',
+                          '#f05b72', '#ef5b9c', '#f47920', '#905a3d', '#fab27b',
+                          '#2a5caa', '#444693', '#726930', '#b2d235', '#6d8346',
+                          '#ac6767', '#1d953f', '#6950a1', '#918597', '#f6f5ec']
         self._option.update(
             title={
                 "text": title,
@@ -142,6 +145,7 @@ class Base(object):
             symbol_size=None,
             symbol=None,
             type=None,
+            visual_orient=None,
             visual_range_color=None,
             visual_range_text=None,
             visual_range=None,
