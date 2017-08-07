fe812ed3f7c75e5e7bb4403f122243de7e55105b
chfw <wangc_2011@hotmail.com>
2017-08-07 19:00:41 +0100

    :umbrella: test numpy int64 array

--- test/test_base.py
+++ test/test_base.py
@@ -21,3 +21,13 @@ def test_embed_option():
     assert json_encoded_title in html
     assert "<html>" not in html
     assert "<body>" not in html
+
+
+def test_numpy_array()
+    import numpy as np
+
+    attr = ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+    v1 = np.array([5, 20, 36, 10, 75, 90])
+    bar = Bar()
+    bar.add("商家A", attr, v1, is_stack=True)
+    bar.render_embed()
