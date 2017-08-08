d569ef09d06036e46ab0d1a6e8f708a64111a91b
chfw <wangc_2011@hotmail.com>
2017-08-08 09:22:21 +0100

    :books: document #9: provide a tutorial on django integration. example codes were included

---
+++ document/django-demo/myechartsite/myfirstvis/views.py
@@ -0,0 +1,30 @@
+from __future__ import unicode_literals
+from django.http import HttpResponse
+from django.template import loader
+
+
+def index(request):
+    template = loader.get_template('myfirstvis/pyecharts.html')
+    context = {
+        "myechart": line3d()
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def line3d():
+    from pyecharts import Line3D
+
+    import math
+    _data = []
+    for t in range(0, 25000):
+        _t = t / 1000
+        x = (1 + 0.25 * math.cos(75 * _t)) * math.cos(_t)
+        y = (1 + 0.25 * math.cos(75 * _t)) * math.sin(_t)
+        z = _t + 2.0 * math.sin(75 * _t)
+        _data.append([x, y, z])
+    range_color = ['#313695', '#4575b4', '#74add1', '#abd9e9', '#e0f3f8', '#ffffbf',
+                   '#fee090', '#fdae61', '#f46d43', '#d73027', '#a50026']
+    line3d = Line3D("3D 折线图示例", width=1200, height=600)
+    line3d.add("", _data, is_visualmap=True, visual_range_color=range_color, visual_range=[0, 30],
+               is_grid3D_rotate=True, grid3D_rotate_speed=180)
+    return line3d.render_embed()
