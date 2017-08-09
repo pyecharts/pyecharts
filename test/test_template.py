b8d38e17c9cd57d3b925f1edc4637fd7621f918c
chfw <wangc_2011@hotmail.com>
2017-08-09 17:35:12 +0100

    :sparkles: #76: support multiple charts in one html page.

--- test/test_template.py
+++ test/test_template.py
@@ -1,4 +1,10 @@
+#coding=utf-8
+from __future__ import unicode_literals
+
+from nose.tools import eq_
 from pyecharts.template import freeze_js
+from pyecharts.template import write_utf8_html_file
+import codecs
 
 
 def test_freeze_js():
@@ -13,3 +19,12 @@ def test_freeze_js():
     html_content = freeze_js(html_content)
     assert 'exports.echarts' in html_content
     assert 'echarts-wordcloud' in html_content
+
+
+def test_write_utf8_html_file():
+    content = "柱状图数据堆叠示例"
+    file_name = 'test.html'
+    write_utf8_html_file(file_name, content)
+    with codecs.open(file_name, 'r', 'utf-8') as f:
+        actual_content = f.read()
+        eq_(content, actual_content)
