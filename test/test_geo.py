0317cd570c4d17a4566727e1520c05403e9c5f0b
chfw <wangc_2011@hotmail.com>
2017-08-13 08:52:37 +0100

    :umbrella: test unknown city name

--- test/test_geo.py
+++ test/test_geo.py
@@ -222,3 +222,12 @@ def test_geo():
     geo.add("", attr, value, type="effectScatter", is_random=True, effect_scale=5)
     geo.show_config()
     geo.render()
+
+    # geo_with_noexist_city
+    data = [("海门", 9), ("鄂尔多斯", 12), ("招远", 12), ("舟山", 12), ("齐齐哈尔", 14), ("伦敦", 15)]
+    geo = Geo("全国主要城市空气质量", "data from pm2.5", title_color="#fff", title_pos="center", width=1200, height=600,
+              background_color='#404a59')
+    attr, value = geo.cast(data)
+    geo.add("", attr, value, type="effectScatter", is_random=True, effect_scale=5)
+    geo.show_config()
+    geo.render()
