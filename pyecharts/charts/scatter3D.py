d0b236ca7bf7eff661c616a0fc16d1ba6638b154
chfw <wangc_2011@hotmail.com>
2017-08-17 22:11:29 +0100

    :tractor: code refactoring remove the chance of duplicated js dependencies. hence set is used

--- pyecharts/charts/scatter3D.py
+++ pyecharts/charts/scatter3D.py
@@ -12,7 +12,7 @@ class Scatter3D(Base):
 
     def __init__(self, title="", subtitle="", **kwargs):
         super(Scatter3D, self).__init__(title, subtitle, **kwargs)
-        self._js_dependencies.append('echartsgl')
+        self._js_dependencies.add('echartsgl')
 
     def add(self, *args, **kwargs):
         self.__add(*args, **kwargs)
