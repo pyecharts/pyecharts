2c243687b1a1a9c17b10b3e0db9fd963552529ce
chfw <wangc_2011@hotmail.com>
2017-08-17 21:47:52 +0100

    :umbrella: test all notebook rendering functions

--- pyecharts/template.py
+++ pyecharts/template.py
@@ -46,43 +46,6 @@ def freeze_js(html_content):
     return html_content
 
 
-CITY_NAME_PINYIN_MAP = {
-    "广东": "guangdong",
-    "安徽": "anhui",
-    "澳门": "aomen",
-    "北京": "beijing",
-    "重庆": "chongqing",
-    "福建": "fujian",
-    "甘肃": "gansu",
-    "广西": "guangxi",
-    "贵州": "guizhou",
-    "海南": "hainan",
-    "河北": "hebei",
-    "黑龙江": "heilongjiang",
-    "河南": "henan",
-    "湖北": "hubei",
-    "湖南": "hunan",
-    "江苏": "jiangsu",
-    "江西": "jiangxi",
-    "吉林": "jilin",
-    "辽宁": "liaoning",
-    "内蒙古": "neimenggu",
-    "宁夏": "ningxia",
-    "青海": "qinghai",
-    "山东": "shandong",
-    "上海": "shanghai",
-    "山西": "shanxi",
-    "四川": "sichuan",
-    "台湾": "taiwan",
-    "天津": "tianjin",
-    "香港": "xianggang",
-    "新疆": "xinjiang",
-    "西藏": "xizang",
-    "云南": "yunnan",
-    "浙江": "zhejiang"
-}
-
-
 def get_resource_dir(folder):
     """
 
