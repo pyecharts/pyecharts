a3f7ead677ca6811588cb55858b804b91df34aea
chenjiandongx <chenjiandongx@qq.com>
2017-08-18 17:24:46 +0800

    add more options for Overlap

--- test/test_overlap.py
+++ test/test_overlap.py
@@ -39,6 +39,7 @@ def test_overlap_1():
     overlap.add(es)
     overlap.add(scatter)
     overlap.add(es_1)
+    overlap.show_config()
     overlap.render()
 
 
@@ -88,4 +89,24 @@ def test_overlap_2():
     overlap.add(kline)
     overlap.add(line_1)
     overlap.add(line_2)
+    overlap.render()
+
+
+def test_overlap_3():
+
+    attr = ["{}月".format(i) for i in range(1, 13)]
+    v1 = [2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
+    v2 = [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
+    v3 = [2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2]
+
+    bar = Bar(width=1200, height=600)
+    bar.add("蒸发量", attr, v1)
+    bar.add("降水量", attr, v2, yaxis_formatter=" ml")
+
+    line = Line()
+    line.add("平均温度", attr, v3, yaxis_formatter=" °C")
+
+    overlap = Overlap()
+    overlap.add(bar)
+    overlap.add(line, yaxis_index=1, is_add_yaxis=True)
     overlap.render()
