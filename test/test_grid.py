a628fcde1b6d6b073099f3bfbc7541f699af6497
chenjiandongx <chenjiandongx@qq.com>
2017-08-24 00:33:30 +0800

    update Grid class documentation

--- test/test_grid.py
+++ test/test_grid.py
@@ -82,11 +82,12 @@ def test_grid_3():
              mark_line=["average"], legend_pos="20%")
     attr = ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
     v1 = [11, 12, 13, 10, 10, 10]
-    pie = Pie("饼图示例", title_pos="45%")
-    pie.add("", attr, v1, radius=[30, 55], legend_pos="65%", legend_orient='vertical')
+    pie = Pie("饼图示例", title_pos="55%")
+    pie.add("", attr, v1, radius=[45, 65], center=[65, 50], legend_pos="80%",
+            legend_orient='vertical')
 
     grid = Grid()
-    grid.add(line, grid_right="65%")
+    grid.add(line, grid_right="55%")
     grid.add(pie, grid_left="60%")
     grid.render()
 
