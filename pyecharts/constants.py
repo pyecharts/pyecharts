efecff9d2e7e13d490109c9a68ccd9d50888508f
chfw <wangc_2011@hotmail.com>
2017-08-28 00:32:52 +0100

    :sparkles: beta support for 331 second-in-line cities. and a dozen more to come.

--- pyecharts/constants.py
+++ pyecharts/constants.py
@@ -2,57 +2,11 @@
 # coding=utf-8
 from __future__ import unicode_literals
 
-
 DEFAULT_HOST = 'https://chfw.github.io/jupyter-echarts/echarts'
 
 CONFIGURATION = dict(
     HOST='/nbextensions/echarts'
 )
-
-DEFAULT_JS_LIBRARIES = dict(
-    echarts='echarts.min',
-    echartsgl='echarts-gl.min',
-    liquidfill='echarts-liquidfill.min',
-    world='world',
-    china='china',
-    wordcloud='echarts-wordcloud.min'
-)
-
-CITY_NAME_PINYIN_MAP = {
-    "广东": "guangdong",
-    "安徽": "anhui",
-    "澳门": "aomen",
-    "北京": "beijing",
-    "重庆": "chongqing",
-    "福建": "fujian",
-    "甘肃": "gansu",
-    "广西": "guangxi",
-    "贵州": "guizhou",
-    "海南": "hainan",
-    "河北": "hebei",
-    "黑龙江": "heilongjiang",
-    "河南": "henan",
-    "湖北": "hubei",
-    "湖南": "hunan",
-    "江苏": "jiangsu",
-    "江西": "jiangxi",
-    "吉林": "jilin",
-    "辽宁": "liaoning",
-    "内蒙古": "neimenggu",
-    "宁夏": "ningxia",
-    "青海": "qinghai",
-    "山东": "shandong",
-    "上海": "shanghai",
-    "山西": "shanxi",
-    "四川": "sichuan",
-    "台湾": "taiwan",
-    "天津": "tianjin",
-    "香港": "xianggang",
-    "新疆": "xinjiang",
-    "西藏": "xizang",
-    "云南": "yunnan",
-    "浙江": "zhejiang"
-}
 
 CITY_GEO_COORDS = {
     '阿城': [126.58, 45.32],
