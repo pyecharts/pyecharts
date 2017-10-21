70be5d11fd460509bb10050914cb00380088c743
chenjiandongx <chenjiandongx@qq.com>
2017-10-21 19:30:06 +0800

    测试代码整理

--- test/test_treemap.py
+++ test/test_treemap.py
@@ -8,54 +8,54 @@ from pyecharts import TreeMap
 
 PY2 = sys.version_info[0] == 2
 
-
-def test_treemap():
-    data = [
-        {
-            "value": 40,
-            "name": "我是A",
-        },
-        {
-            "value": 180,
-            "name": "我是B",
-            "children": [
-                {
-                    "value": 76,
-                    "name": "我是B.children",
-                    "children": [
-                        {
-                            "value": 12,
-                            "name": "我是B.children.a",
-                        },
-                        {
-                            "value": 28,
-                            "name": "我是B.children.b",
-                        },
-                        {
-                            "value": 20,
-                            "name": "我是B.children.c",
-                        },
-                        {
-                            "value": 16,
-                            "name": "我是B.children.d",
-                        }]
-                }]}
+data = [
+    {
+        "value": 40,
+        "name": "我是A",
+    },
+    {
+        "value": 180,
+        "name": "我是B",
+        "children": [
+            {
+                "value": 76,
+                "name": "我是B.children",
+                "children": [
+                    {
+                        "value": 12,
+                        "name": "我是B.children.a",
+                    },
+                    {
+                        "value": 28,
+                        "name": "我是B.children.b",
+                    },
+                    {
+                        "value": 20,
+                        "name": "我是B.children.c",
+                    },
+                    {
+                        "value": 16,
+                        "name": "我是B.children.d",
+                    }]
+            }]}
     ]
 
-    # 默认示例
-    treemap = TreeMap("树图示例", width=1200, height=600)
+
+def test_treemap_default():
+    treemap = TreeMap("树图-默认示例", width=1200, height=600)
     treemap.add("演示数据", data, is_label_show=True, label_pos='inside')
     treemap.render()
 
-    # 下钻示例
-    treemap = TreeMap("树图示例", width=1200, height=600)
+
+def test_treemap_drilldown():
+    treemap = TreeMap("树图-下钻示例", width=1200, height=600)
     treemap.add("演示数据", data, is_label_show=True, label_pos='inside',
                 treemap_left_depth=1)
     treemap.render()
 
 
-def test_treemap_big_data():
-    treemap = TreeMap("树图示例", width=1200, height=600)
+def test_treemap_offcical_data():
+    treemap = TreeMap("树图-官方数据", width=1200, height=600)
     import os
     import json
     if PY2:
@@ -63,7 +63,8 @@ def test_treemap_big_data():
         with codecs.open(os.path.join("..", "json", "treemap.json"), "rb") as f:
             data = json.load(f)
     else:
-        with open(os.path.join("..", "json", "treemap.json"), "r", encoding="utf-8") as f:
+        with open(os.path.join("..", "json", "treemap.json"),
+                  "r", encoding="utf-8") as f:
             data = json.load(f)
     treemap.add("演示数据", data, is_label_show=True, label_pos='inside')
     treemap.render()
