c531549da66e44af77915655ccb724804c046fc8
kinegratii <kinegratii@gmail.com>
2017-10-30 11:47:46 +0800

    Update render functions

---
+++ test/test_conf.py
@@ -0,0 +1,24 @@
+# coding=utf8
+
+from __future__ import unicode_literals
+
+from pyecharts.conf import PyEchartsConfig
+
+
+def test_config():
+    pec = PyEchartsConfig(jshost='https://demo')
+    assert pec.jshost == 'https://demo'
+    pec.jshost = 'https://demo/'
+    assert pec.jshost == 'https://demo'
+
+    pec.force_js_embed = True
+    assert pec.js_embed
+    pec.force_js_embed = False
+    assert not pec.js_embed
+
+    pec.jshost = '/templates/js/'
+    assert pec.jshost == '/templates/js'
+    pec.force_js_embed = True
+    assert pec.js_embed
+    pec.force_js_embed = False
+    assert pec.js_embed
