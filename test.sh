24aeeee7695929f1d51fffb1e2caec72f2d45b4e
chfw <wangc_2011@hotmail.com>
2017-11-17 21:50:57 +0000

    :shirt: unit test succeeds first and then do flake8

--- test.sh
+++ test.sh
@@ -1,3 +1,2 @@
 cd test
-nosetests --with-coverage --cover-package pyecharts --cover-package test
-flake8 pyecharts --builtins=unicode,xrange,long
+nosetests --with-coverage --cover-package pyecharts --cover-package test && flake8 pyecharts --builtins=unicode,xrange,long
