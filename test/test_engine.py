b07ce13eda509c362422aa715a9ac18c847ca024
chfw <wangc_2011@hotmail.com>
2017-12-09 22:36:39 +0000

    :tractor: code refactoring - engine, template and constants

--- test/test_engine.py
+++ test/test_engine.py
@@ -2,16 +2,17 @@
 
 from __future__ import unicode_literals
 
-from pyecharts.engine import Helpers
+import pyecharts.engine as engine
 
 from nose.tools import eq_
 
 
 def test_merge_js_dependencies():
-    # Helpers.merge_js_dependencies(str1, str2,...)
-    eq_(['echarts', 'fujian'], Helpers.merge_js_dependencies('echarts', 'fujian'))
+    # engine.merge_js_dependencies(str1, str2,...)
+    eq_(['echarts', 'fujian'],
+        engine.merge_js_dependencies('echarts', 'fujian'))
 
-    # Helpers.merge_js_dependencies(chart)
+    # engine.merge_js_dependencies(chart)
     class MockChart(object):
         def __init__(self, js_dependencies):
             self.js_dependencies = js_dependencies
@@ -19,23 +20,23 @@ def test_merge_js_dependencies():
     ch1 = MockChart(['echarts', 'fujian', 'zhengjiang', 'anhui'])
     eq_(
         ['echarts', 'fujian', 'zhengjiang', 'anhui'],
-        Helpers.merge_js_dependencies(ch1)
+        engine.merge_js_dependencies(ch1)
     )
 
-    # Helpers.merge_js_dependencies(chart1, chart2 )
+    # engine.merge_js_dependencies(chart1, chart2 )
     ch1 = MockChart(['echarts'])
     ch2 = MockChart(['echarts', 'beijing'])
     eq_(
         ['echarts', 'beijing'],
-        Helpers.merge_js_dependencies(ch1, ch2)
+        engine.merge_js_dependencies(ch1, ch2)
     )
 
-    # Helpers.merge_js_dependencies(*chart_list)
+    # engine.merge_js_dependencies(*chart_list)
 
     mock_page = [ch1, ch2]
     ch1 = MockChart(['echarts'])
     ch2 = MockChart(['echarts', 'beijing'])
     eq_(
         ['echarts', 'beijing'],
-        Helpers.merge_js_dependencies(*mock_page)
+        engine.merge_js_dependencies(*mock_page)
     )
