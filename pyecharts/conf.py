920cdabeed6c18c79134d8a73e198dd027d52416
chfw <wangc_2011@hotmail.com>
2017-12-09 22:36:39 +0000

    :tractor: code refactoring - engine, template and constants

--- pyecharts/conf.py
+++ pyecharts/conf.py
@@ -1,8 +1,28 @@
 # coding=utf-8
 from __future__ import unicode_literals
 
-from pyecharts.constants import SCRIPT_LOCAL_JSHOST, JUPYTER_LOCAL_JSHOST, \
-    DEFAULT_HOST
+import os
+import json
+import codecs
+
+from pyecharts.utils import get_resource_dir
+import pyecharts.constants as constants
+
+SCRIPT_LOCAL_JSHOST = get_resource_dir('templates', 'js', 'echarts')
+# Path constants for template dir
+
+DEFAULT_TEMPLATE_DIR = get_resource_dir('templates')
+DEFAULT_ECHARTS_REGISTRY = os.path.join(
+    get_resource_dir('templates'), 'js', 'echarts', 'registry.json')
+# Load js & map file index into a dictionary.
+
+
+with codecs.open(DEFAULT_ECHARTS_REGISTRY, 'r', 'utf-8') as f:
+    content = f.read()
+    CONFIG = json.loads(content)
+
+DEFAULT_JS_LIBRARIES = CONFIG['FILE_MAP']  # {<Pinyin>:<Js File Name>}
+CITY_NAME_PINYIN_MAP = CONFIG['PINYIN_MAP']  # {<Chinese Name>:<Pinyin>}
 
 
 class PyEchartsConfig(object):
@@ -21,7 +41,8 @@ class PyEchartsConfig(object):
         if self.force_js_embed:
             return True
         else:
-            return self._jshost in (SCRIPT_LOCAL_JSHOST, DEFAULT_HOST)
+            return self._jshost in (
+                SCRIPT_LOCAL_JSHOST, constants.DEFAULT_HOST)
 
     @property
     def jshost(self):
@@ -47,7 +68,7 @@ class PyEchartsConfig(object):
         """
         jshost = jshost or self.jshost
         if jshost == SCRIPT_LOCAL_JSHOST:
-            return JUPYTER_LOCAL_JSHOST
+            return constants.JUPYTER_LOCAL_JSHOST
         else:
             return jshost
 
@@ -61,3 +82,12 @@ class PyEchartsConfig(object):
         if jshost[-1:] in ('/', '\\'):
             jshost = jshost[:-1]
         return jshost
+
+    def get_js_library(self, pinyin):
+        return DEFAULT_JS_LIBRARIES.get(pinyin, pinyin)
+
+    def chinese_to_pinyin(self, chinese):
+        return CITY_NAME_PINYIN_MAP.get(chinese, chinese)
+
+
+CURRENT_CONFIG = PyEchartsConfig()
