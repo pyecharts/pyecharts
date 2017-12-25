618001b65ec0b1bc48eaf9445f168eac0388930b
chenjiandongx <chenjiandongx@qq.com>
2017-12-25 13:56:50 +0800

    新增测试

--- test/test_timeline.py
+++ test/test_timeline.py
@@ -130,3 +130,22 @@ def test_timeline_bar_line():
     timeline.add(overlap_3, '4 月')
     timeline.add(overlap_4, '5 月')
     timeline.render()
+
+
+def test_timeline_map():
+    from pyecharts import Map
+    timeline = Timeline(timeline_bottom=0)
+    value = [155, 10, 66, 78, 33, 80, 190, 53, 49.6]
+    attr = ["福建", "山东", "北京", "上海", "甘肃",
+            "新疆", "河南", "广西", "西藏"]
+    map = Map("Map 结合 VisualMap 示例", width=1200, height=600)
+    map.add("", attr, value, maptype='china', is_visualmap=True,
+            visual_text_color='#000', visual_top="30%")
+    timeline.add(map, "test1")
+    value = [155, 10, 66, 78, 33]
+    attr = ["福建", "山东", "北京", "上海", "甘肃"]
+    map = Map("Map 结合 VisualMap 示例", width=1200, height=600)
+    map.add("", attr, value, maptype='china', is_visualmap=True,
+            visual_text_color='#000', visual_top="30%")
+    timeline.add(map, "test2")
+    timeline.render()
