efef71644e945d30f813c751c46bcf0c5f8e342c
kinegratii <kinegratii@gmail.com>
2018-01-06 21:27:20 +0800

    Refactor chart's  js_dependencies
    1. Now chart.js_dependencies returns list instead of set
    2. Expose merge_js_dependencies as utils API

--- test/test_utils.py
+++ test/test_utils.py
@@ -11,7 +11,8 @@ from nose.tools import eq_
 from pyecharts.utils import (
     write_utf8_html_file,
     get_resource_dir,
-    json_dumps
+    json_dumps,
+    merge_js_dependencies
 )
 
 
@@ -42,3 +43,38 @@ def test_json_encoder():
     data2 = {'np_list': np.array(['a', 'b', 'c'])}
     data2_e = {'np_list': ['a', 'b', 'c']}
     eq_(json.dumps(data2_e, indent=0), json_dumps(data2))
+
+
+def test_merge_js_dependencies():
+    # PyEchartsConfig.merge_js_dependencies(str1, str2,...)
+    eq_(['echarts', 'fujian'],
+        merge_js_dependencies('echarts', 'fujian'))
+
+    # PyEchartsConfig.merge_js_dependencies(chart)
+    class MockChart(object):
+        def __init__(self, js_dependencies):
+            self.js_dependencies = js_dependencies
+
+    ch1 = MockChart(['echarts', 'fujian', 'zhengjiang', 'anhui'])
+    eq_(
+        ['echarts', 'fujian', 'zhengjiang', 'anhui'],
+        merge_js_dependencies(ch1)
+    )
+
+    # PyEchartsConfig.merge_js_dependencies(chart1, chart2 )
+    ch1 = MockChart(['echarts'])
+    ch2 = MockChart(['echarts', 'beijing'])
+    eq_(
+        ['echarts', 'beijing'],
+        merge_js_dependencies(ch1, ch2)
+    )
+
+    # PyEchartsConfig.merge_js_dependencies(*chart_list)
+
+    mock_page = [ch1, ch2]
+    ch1 = MockChart(['echarts'])
+    ch2 = MockChart(['echarts', 'beijing'])
+    eq_(
+        ['echarts', 'beijing'],
+        merge_js_dependencies(*mock_page)
+    )
