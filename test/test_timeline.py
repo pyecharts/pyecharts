c3b30839694dd6bbe5cf0afa089707062eb687e7
chenjiandongx <chenjiandongx@qq.com>
2018-01-12 22:08:00 +0800

    测试修复

--- test/test_timeline.py
+++ test/test_timeline.py
@@ -172,5 +172,5 @@ def test_timeline_different_legend():
     timeline.add(bar_1, '2012 年')
     timeline.add(bar_2, '2013 年')
     content = timeline._repr_html_()
-    assert r"\u6625\u5b63a" in content      # 春季 a
-    assert r"\u6625\u5b63b" in content      # 春季 b
+    assert "\\u6625\\u5b63a" in content      # 春季 a
+    assert "\\u6625\\u5b63b" in content      # 春季 b
