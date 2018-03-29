4ada9b889f5976e262fec8230563655b4d61dfa6
kinegratii <kinegratii@gmail.com>
2018-03-29 15:58:40 +0800

    Move coordinate data to local data files.

--- pyecharts/utils.py
+++ pyecharts/utils.py
@@ -4,7 +4,11 @@ from __future__ import unicode_literals
 import codecs
 import datetime
 import os
+import sys
 import json
+import operator
+
+PY2 = sys.version_info[0] == 2
 
 
 def get_resource_dir(*paths):
@@ -104,3 +108,45 @@ def merge_js_dependencies(*chart_or_name_list):
     # items which should be included in front part.
     fol = [x for x in front_optional_items if x in fist_items]
     return front_must_items + fol + dependencies
+
+
+# ------- Lazy Module ------#
+
+def proxy_method(func):
+    def inner(self, *args):
+        if self._wrapped is None:
+            self._setup()
+        return func(self._wrapped, *args)
+
+    return inner
+
+
+class LazyObject(object):
+    _wrapped = None
+
+    def __init__(self, func):
+        self.__dict__['_setupfunc'] = func
+
+    __getattr__ = proxy_method(getattr)
+
+    def __setattr__(self, key, value):
+        if key == '_wrapped':
+            self.__dict__['_wrapped'] = value
+        else:
+            if self._wrapped is None:
+                self._setup()
+            setattr(self._wrapped, key, value)
+
+    def _setup(self):
+        self._wrapped = self._setupfunc()
+
+    __getitem__ = proxy_method(operator.getitem)
+
+    if PY2:
+        __str__ = proxy_method(str)
+        __unicode__ = proxy_method(unicode)  # NOQA: unicode undefined on PY3
+        __nonzero__ = proxy_method(bool)
+    else:
+        __bytes__ = proxy_method(bytes)
+        __str__ = proxy_method(str)
+        __bool__ = proxy_method(bool)
