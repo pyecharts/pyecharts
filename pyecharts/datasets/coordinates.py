9626a37e263992b66b76453cc3caea1038ccb8f8
chfw <wangc_2011@hotmail.com>
2018-04-24 20:16:32 +0100

    :tractor: rename city coordinates.json

--- pyecharts/datasets/coordinates.py
+++ pyecharts/datasets/coordinates.py
@@ -51,7 +51,7 @@ def _load_coordinates():
     :return:
     """
     with open(
-        get_resource_dir('datasets', 'CityCoordinates.json'), encoding='utf8'
+        get_resource_dir('datasets', 'city_coordinates.json'), encoding='utf8'
     ) as f:
         return json.load(f)
 
