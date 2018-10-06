c4b47df29806c22f222f566067575b85d52da5b3
kinegratii <kinegratii@gmail.com>
2018-10-06 21:30:47 +0800

    :bug: FIx broken tests

--- pyecharts/base.py
+++ pyecharts/base.py
@@ -77,9 +77,17 @@ class Base(IPythonRichDisplayMixin):
     def width(self):
         return self._width
 
+    @width.setter
+    def width(self, width):
+        self._width = width
+
     @property
     def height(self):
         return self._height
+
+    @height.setter
+    def height(self, height):
+        self._height = height
 
     @property
     def page_title(self):
