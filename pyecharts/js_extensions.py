a4af8e4c8e357716b0ef4774cf619e4d39c3f4a9
chfw <wangc_2011@hotmail.com>
2018-11-12 07:13:40 +0000

    :hammer: code refactoring and addressing review comments

--- pyecharts/js_extensions.py
+++ pyecharts/js_extensions.py
@@ -10,10 +10,10 @@ from lml.plugin import PluginManager
 import pyecharts.exceptions as exceptions
 
 # here are all plugins from pyecharts team
-OFFICIAL_PLUGINS = ["jupyter_echarts_pypkg"]
+MUST_LOAD_PLUGINS = ["jupyter_echarts_pypkg"]
 PYTHON_PLUGINS = "^pyecharts_.+$"
 JS_PLUGINS = "^echarts_.+$"
-THIRD_PARTY_PLUGIN_PREFIX = "|".join([PYTHON_PLUGINS, JS_PLUGINS])
+OPTIONAL_PLUGIN_PREFIX = "|".join([PYTHON_PLUGINS, JS_PLUGINS])
 
 JS_EXTENSION_PLUGIN_TYPE = "pyecharts_js_extension"
 JS_EXTENSION_REGISTRY = "registry.json"
@@ -118,9 +118,9 @@ class JsExtensionManager(PluginManager):
 EXTENSION_MANAGER = JsExtensionManager()
 # Load js & map file index into a dictionary.
 scan_plugins_regex(
-    THIRD_PARTY_PLUGIN_PREFIX,
-    "pyecharts",  # <- useful for pyinstaller only
-    white_list=OFFICIAL_PLUGINS,
+    plugin_name_patterns=OPTIONAL_PLUGIN_PREFIX,
+    pyinstaller_path="pyecharts",  # <- useful for pyinstaller only
+    white_list=MUST_LOAD_PLUGINS,
 )
 
 
