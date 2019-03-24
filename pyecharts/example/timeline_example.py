e45a16b6833224cde0322b7fc130f1cfdf9f03a4
陈键冬 <chenjiandongx@qq.com>
2019-03-24 18:16:23 +0800

    Fix: 修复 timeline 不能正常显示的 bug

--- pyecharts/example/timeline_example.py
+++ pyecharts/example/timeline_example.py
@@ -1,21 +1,56 @@
+# coding=utf-8
 from pyecharts import options as opts
-from pyecharts.charts import Bar, Timeline
+from pyecharts.charts import Bar, Timeline, Page
+from pyecharts.example.commons import Faker
 
-bar_attr = ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
-v1 = [5, 20, 36, 10, 75, 90]
-v2 = [10, 25, 8, 60, 20, 80]
-bar1 = Bar()
-bar1.add_xaxis(bar_attr).add_yaxis("商家A", v1).add_yaxis("商家B", v2).set_global_opts(
-    title_opts={"text": "2018asdasdasd"}
-)
+charts = []
 
-bar_attr = ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
-v1 = [5, 20, 36, 10, 75, 901]
-v2 = [10, 25, 8, 60, 20, 801]
-bar2 = Bar()
-bar2.add_xaxis(bar_attr).add_yaxis("商家A", v1).add_yaxis("商家B", v2).set_global_opts(
-    title_opts={"text": "2019asdasdasd"}
-)
 
-t = Timeline()
-t.add(bar1, "2018").add(bar2, "2019").render()
+def collect_charts(fn):
+    charts.append((fn, fn.__name__))
+    return fn
+
+
+@collect_charts
+def timeline_base() -> Timeline:
+    x = Faker.choose()
+    bar1 = (
+        Bar()
+        .add_xaxis(x)
+        .add_yaxis("商家A", Faker.values())
+        .add_yaxis("商家B", Faker.values())
+        .set_global_opts(title_opts=opts.TitleOpts("某商店2016年营业额"))
+    )
+    bar2 = (
+        Bar()
+        .add_xaxis(x)
+        .add_yaxis("商家A", Faker.values())
+        .add_yaxis("商家B", Faker.values())
+        .set_global_opts(title_opts=opts.TitleOpts("某商店2017年营业额"))
+    )
+    bar3 = (
+        Bar()
+        .add_xaxis(x)
+        .add_yaxis("商家A", Faker.values())
+        .add_yaxis("商家B", Faker.values())
+        .set_global_opts(title_opts=opts.TitleOpts("某商店2018年营业额"))
+    )
+    bar4 = (
+        Bar()
+        .add_xaxis(x)
+        .add_yaxis("商家A", Faker.values())
+        .add_yaxis("商家B", Faker.values())
+        .set_global_opts(title_opts=opts.TitleOpts("某商店2019年营业额"))
+    )
+
+    tl = (
+        Timeline()
+        .add(bar1, "2016年")
+        .add(bar2, "2017年")
+        .add(bar3, "2018年")
+        .add(bar4, "2019年")
+    )
+    return tl
+
+
+Page().add(*[fn() for fn, _ in charts]).render()
