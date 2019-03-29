77f2ded62f0a6e8d89af8e5bfc16dfb194e0cfd7
chenjiandongx <chenjiandongx@qq.com>
2019-03-29 14:09:46 +0800

    Remove: unused images

--- install.bat
+++ install.bat
@@ -1,4 +1,3 @@
 python setup.py bdist_wheel
 pip uninstall pyecharts -y
-pip install -U dist\pyecharts-1.0.0-py2.py3-none-any.whl --no-cache-dir
-
+pip install -U dist/pyecharts-1.0.0-py2.py3-none-any.whl --no-cache-dir
