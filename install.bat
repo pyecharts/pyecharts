e0ce5a20a2097745d15bc347263888546b1ee356
陈键冬 <chenjiandongx@qq.com>
2019-04-08 00:44:22 +0800

    Add: install.py

--- install.bat
+++
@@ -1,3 +0,0 @@
-python setup.py bdist_wheel
-pip uninstall pyecharts -y
-pip install -U dist/pyecharts-1.0.0-py2.py3-none-any.whl --no-cache-dir
---
+++ install.py
@@ -0,0 +1,14 @@
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+about = {}
+with open(os.path.join(here, "pyecharts", "_version.py")) as f:
+    exec(f.read(), about)
+
+os.system("python setup.py bdist_wheel")
+os.system("pip uninstall pyecharts -y")
+os.system(
+    "pip install -U dist/pyecharts-{}-py2.py3-none-any.whl --no-cache-dir".format(
+        about["__version__"]
+    )
+)
