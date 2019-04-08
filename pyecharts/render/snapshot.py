bbd9df066da54711b7ad34a522596f42072bfbd3
陈键冬 <chenjiandongx@qq.com>
2019-04-08 14:24:08 +0800

    Update: remove async js code

--- pyecharts/render/snapshot.py
+++ pyecharts/render/snapshot.py
@@ -5,8 +5,6 @@ import sys
 import os
 
 from selenium import webdriver
-from selenium.common import exceptions
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
 def make_snapshot(
@@ -15,34 +13,27 @@ def make_snapshot(
     pixel_ratio: int = 2,
     delay: int = 2,
     is_remove_html: bool = True,
-    browser='Chrome'
+    browser: str = "Chrome",
 ):
     if delay < 0:
-        raise Exception('Time travel is not possible')
-    if browser == 'Chrome':
+        raise Exception("Time travel is not possible")
+
+    if browser == "Chrome":
         driver = get_chrome()
-    elif browser == 'Safari':
+    elif browser == "Safari":
         driver = get_safari()
     else:
-        raise Exception('Unknown browser!')
-    driver.set_script_timeout(delay + 1)
+        raise Exception("Unknown browser!")
 
     if not html_path.startswith("http"):
-        html_path = 'file://' + os.path.abspath(html_path)
+        html_path = "file://" + os.path.abspath(html_path)
+
     driver.get(html_path)
     time.sleep(delay)
 
     ext = image_name.split(".")[1]
-
-    try:
-        output = driver.execute_script(__gen_js_code(ext, pixel_ratio, delay))
-    except exceptions.TimeoutException:
-        pass
-
-    try:
-        output = output.split(",")[1]
-    except:
-        raise
+    output = driver.execute_script(__gen_js_code(ext, pixel_ratio))
+    output = output.split(",")[1]
 
     with open(image_name, "wb") as f:
         f.write(base64.b64decode(output))
@@ -53,17 +44,16 @@ def make_snapshot(
     print("render {}".format(image_name))
 
 
-def __gen_js_code(file_type: str, pixel_ratio: int, delay: int) -> str:
-    script = (
-        """
+def __gen_js_code(file_type: str, pixel_ratio: int) -> str:
+    script = """
         var ele = document.querySelector('div[_echarts_instance_]');
         var mychart = echarts.getInstanceByDom(ele);
         return mychart.getDataURL(
             {type:'--file-type--', pixelRatio: --pixel-ratio--, excludeComponents: ['toolbox']});
     """.replace(
-            "--file-type--", file_type
-        )
-        .replace("--pixel-ratio--", str(pixel_ratio))
+        "--file-type--", file_type
+    ).replace(
+        "--pixel-ratio--", str(pixel_ratio)
     )
     return script
 
@@ -71,14 +61,12 @@ def __gen_js_code(file_type: str, pixel_ratio: int, delay: int) -> str:
 def get_chrome():
     option = webdriver.ChromeOptions()
     option.add_argument("headless")
-    if sys.platform == 'darwin':
-        option.binary_location = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
-    capabilities = DesiredCapabilities.CHROME
-    capabilities["loggingPrefs"] = {"browser": "ALL"}
-    return webdriver.Chrome(
-        options=option,
-        desired_capabilities=capabilities)
+    if sys.platform == "darwin":
+        option.binary_location = (
+            "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+        )
+    return webdriver.Chrome(options=option)
 
 
 def get_safari():
-    return webdriver.Safari(executable_path='/usr/bin/safaridriver')
+    return webdriver.Safari()
