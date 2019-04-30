6bdaa33d560583ecb74adf57fa5d59992c671d0d
陈键冬 <chenjiandongx@qq.com>
2019-04-30 23:36:09 +0800

    Update: 精简配置项

--- example/line_example.py
+++ example/line_example.py
@@ -92,21 +92,11 @@ def line_itemstyle() -> Line:
             symbol="triangle",
             symbol_size=20,
             linestyle_opts=opts.LineStyleOpts(color="green", width=4, type_="dashed"),
-            label_opts=opts.LabelOpts(is_show=False),
             itemstyle_opts=opts.ItemStyleOpts(
                 border_width=3, border_color="yellow", color="blue"
             ),
         )
-        .set_global_opts(
-            title_opts=opts.TitleOpts(title="Line-ItemStyle"),
-            xaxis_opts=opts.AxisOpts(type_="category"),
-            yaxis_opts=opts.AxisOpts(
-                type_="value",
-                axistick_opts=opts.AxisTickOpts(is_show=True),
-                splitline_opts=opts.SplitLineOpts(is_show=True),
-            ),
-            tooltip_opts=opts.TooltipOpts(is_show=False),
-        )
+        .set_global_opts(title_opts=opts.TitleOpts(title="Line-ItemStyle"))
     )
     return c
 
