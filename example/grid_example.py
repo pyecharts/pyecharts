e151e8581aef8c2be51f6efce546edbdd7f75b7b
陈键冬 <chenjiandongx@qq.com>
2019-05-01 00:06:53 +0800

    Add: grid example

--- example/grid_example.py
+++ example/grid_example.py
@@ -65,4 +65,84 @@ def grid_horizontal() -> Grid:
     return grid
 
 
+@C.funcs
+def grid_mutil_yaxis() -> Grid:
+    x_data = ["{}月".format(i) for i in range(1, 13)]
+    bar = (
+        Bar()
+        .add_xaxis(x_data)
+        .add_yaxis(
+            "蒸发量",
+            [2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3],
+            yaxis_index=0,
+            color="#d14a61",
+        )
+        .add_yaxis(
+            "降水量",
+            [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3],
+            yaxis_index=1,
+            color="#5793f3",
+        )
+        .extend_axis(
+            yaxis=opts.AxisOpts(
+                name="蒸发量",
+                type_="value",
+                min_=0,
+                max_=250,
+                position="right",
+                axisline_opts=opts.AxisLineOpts(
+                    linestyle_opts=opts.LineStyleOpts(color="#d14a61")
+                ),
+                axislabel_opts=opts.LabelOpts(formatter="{value} ml"),
+            )
+        )
+        .extend_axis(
+            yaxis=opts.AxisOpts(
+                type_="value",
+                name="温度",
+                min_=0,
+                max_=25,
+                position="left",
+                axisline_opts=opts.AxisLineOpts(
+                    linestyle_opts=opts.LineStyleOpts(color="#675bba")
+                ),
+                axislabel_opts=opts.LabelOpts(formatter="{value} °C"),
+                splitline_opts=opts.SplitLineOpts(
+                    is_show=True, linestyle_opts=opts.LineStyleOpts(opacity=1)
+                ),
+            )
+        )
+        .set_global_opts(
+            yaxis_opts=opts.AxisOpts(
+                name="降水量",
+                min_=0,
+                max_=250,
+                position="right",
+                offset=80,
+                axisline_opts=opts.AxisLineOpts(
+                    linestyle_opts=opts.LineStyleOpts(color="#5793f3")
+                ),
+                axislabel_opts=opts.LabelOpts(formatter="{value} ml"),
+            ),
+            title_opts=opts.TitleOpts(title="Grid-多 Y 轴示例"),
+            tooltip_opts=opts.TooltipOpts(trigger="axis", axis_pointer_type="cross"),
+        )
+    )
+
+    line = (
+        Line()
+        .add_xaxis(x_data)
+        .add_yaxis(
+            "平均温度",
+            [2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2],
+            yaxis_index=2,
+            color="#675bba",
+            label_opts=opts.LabelOpts(is_show=False),
+        )
+    )
+
+    bar.overlap(line)
+    return Grid().add(bar, opts.GridOpts(pos_left="5%", pos_right="20%"))
+
+
 Page().add(*[fn() for fn, _ in C.charts]).render()
