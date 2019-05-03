b9a82db557961c340093003ebc8cc75a6a9aecba
sunhailinLeo <379978424@qq.com>
2019-05-03 08:13:40 +0800

    Fix pie_example (CI line too long error)

--- example/pie_example.py
+++ example/pie_example.py
@@ -72,17 +72,34 @@ def pie_rich_label() -> Pie:
             radius=["40%", "55%"],
             label_opts=opts.LabelOpts(
                 position="outside",
-                formatter='{a|{a}}{abg|}\n{hr|}\n {b|{b}: }{c}  {per|{d}%}  ',
+                formatter="{a|{a}}{abg|}\n{hr|}\n {b|{b}: }{c}  {per|{d}%}  ",
                 background_color="#eee",
                 border_color="#aaa",
                 border_width=1,
                 border_radius=4,
-                rich={"a": {"color": "#999", "lineHeight": 22, "align": "center"},
-                      "abg": {"backgroundColor": "#e3e3e3", "width": "100%", "align": "right", "height": 22,
-                              "borderRadius": [4, 4, 0, 0]},
-                      "hr": {"borderColor": "#aaa", "width": "100%", "borderWidth": 0.5, "height": 0},
-                      "b": {"fontSize": 16, "lineHeight": 33},
-                      "per": {"color": "#eee", "backgroundColor": "#334455", "padding": [2, 4], "borderRadius": 2}}
+                rich={
+                    "a": {"color": "#999", "lineHeight": 22, "align": "center"},
+                    "abg": {
+                        "backgroundColor": "#e3e3e3",
+                        "width": "100%",
+                        "align": "right",
+                        "height": 22,
+                        "borderRadius": [4, 4, 0, 0],
+                    },
+                    "hr": {
+                        "borderColor": "#aaa",
+                        "width": "100%",
+                        "borderWidth": 0.5,
+                        "height": 0,
+                    },
+                    "b": {"fontSize": 16, "lineHeight": 33},
+                    "per": {
+                        "color": "#eee",
+                        "backgroundColor": "#334455",
+                        "padding": [2, 4],
+                        "borderRadius": 2,
+                    },
+                },
             ),
         )
         .set_global_opts(title_opts=opts.TitleOpts(title="Pie-富文本示例"))
