f2f406ee71f67c292e2cc383952ed3903732ce15
sunhailinLeo <379978424@qq.com>
2019-05-05 18:11:41 +0800

    Fix CI error (E501 line too long)

--- pyecharts/options/series_options.py
+++ pyecharts/options/series_options.py
@@ -494,7 +494,9 @@ class BMapCopyrightType(BasicOpts):
         position: Union[BMapType, Numeric] = BMapType.BMAP_ANCHOR_BOTTOM_LEFT,
         offset_width: Numeric = 2,
         offset_height: Numeric = 2,
-        copy_right: str = "© 2019 pyecharts <a href='https://github.com/pyecharts/pyecharts'>pyecharts repository</a>",
+        copy_right: str =
+            "© 2019 {0} <a href='https://github.com/{0}/{0}'>{0} repository</a>"
+            .format("pyecharts"),
     ):
         self.opts: dict = {
             "functions": [
