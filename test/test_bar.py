07781b7d4316efde3f1118c3d0cfc3a426def368
chenjiandongx <chenjiandongx@qq.com>
2019-05-08 10:09:58 +0800

    Update: set_series_opts 新增 kwargs，允许传入任意变量

--- test/test_bar.py
+++ test/test_bar.py
@@ -1,3 +1,4 @@
+import re
 from unittest.mock import patch
 
 from nose.tools import assert_in, assert_not_in, eq_
@@ -22,11 +23,27 @@ def test_bar_base():
 def test_bar_colors(fake_writer):
     c = Bar().add_xaxis(["A", "B", "C"]).add_yaxis("series0", [1, 2, 4])
     c.set_colors(["#AABBCC", "#BBCCDD", "#CCDDEE"] + c.colors)
-    c.render("render.html")
+    c.render()
     _, content = fake_writer.call_args[0]
     assert_in("#AABBCC", content)
     assert_in("#BBCCDD", content)
     assert_in("#CCDDEE", content)
+
+
+@patch("pyecharts.render.engine.write_utf8_html_file")
+def test_bar_series_stack(fake_writer):
+    c = (
+        Bar()
+        .add_xaxis(["A", "B", "C"])
+        .add_yaxis("series0", [1, 2, 4])
+        .add_yaxis("series1", [2, 3, 6])
+        .add_yaxis("series2", [5, 8, 7])
+        .set_series_opts(stack="MY_STACK_NAME")
+    )
+    c.render()
+    _, content = fake_writer.call_args[0]
+    stack_cnt = re.findall("MY_STACK_NAME", content)
+    eq_(3, len(stack_cnt))
 
 
 @patch("pyecharts.render.engine.write_utf8_html_file")
@@ -41,7 +58,7 @@ def test_bar_title_options(fake_writer):
             )
         )
     )
-    c.render("render.html")
+    c.render()
     file_name, content = fake_writer.call_args[0]
     eq_("render.html", file_name)
     assert_in("This is title.", content)
