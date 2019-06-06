0c10382a7a01fff6601a162f1e91c49142dad270
chenjiandongx <chenjiandongx@qq.com>
2019-06-06 14:42:34 +0800

    Add: more tests

--- test/test_funnel.py
+++ test/test_funnel.py
@@ -1,11 +1,15 @@
+from unittest.mock import patch
+
 from nose.tools import eq_
 
 from example.commons import Faker
 from pyecharts.charts import Funnel
 
 
-def test_funnel_base():
+@patch("pyecharts.render.engine.write_utf8_html_file")
+def test_funnel_base(fake_writer):
     c = Funnel().add("商品", [list(z) for z in zip(Faker.choose(), Faker.values())])
+    c.render()
+    _, content = fake_writer.call_args[0]
     eq_(c.theme, "white")
     eq_(c.renderer, "canvas")
-    c.render()
--- test/test_map.py
+++ test/test_map.py
@@ -1,13 +1,17 @@
+from unittest.mock import patch
+
 from nose.tools import eq_
 
 from example.commons import Faker
 from pyecharts.charts import Map
 
 
-def test_map_base():
+@patch("pyecharts.render.engine.write_utf8_html_file")
+def test_map_base(fake_writer):
     c = Map().add(
         "商家A", [list(z) for z in zip(Faker.provinces, Faker.values())], "china"
     )
+    c.render()
+    _, content = fake_writer.call_args[0]
     eq_(c.theme, "white")
     eq_(c.renderer, "canvas")
-    c.render()
