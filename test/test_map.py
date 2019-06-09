2ddff120d899a3fd1012739b06234926e632e10c
chfw <wangc_2011@hotmail.com>
2019-06-09 13:55:58 +0100

    :sparkles: add emphasis to map

--- test/test_map.py
+++ test/test_map.py
@@ -1,9 +1,11 @@
 from unittest.mock import patch
 
+import json
 from nose.tools import eq_
 
 from example.commons import Faker
 from pyecharts.charts import Map
+from pyecharts import options as opts
 
 
 @patch("pyecharts.render.engine.write_utf8_html_file")
@@ -15,3 +17,19 @@ def test_map_base(fake_writer):
     _, content = fake_writer.call_args[0]
     eq_(c.theme, "white")
     eq_(c.renderer, "canvas")
+
+
+def test_map_emphasis():
+    c = Map().add(
+        "商家A",
+        [list(z) for z in zip(Faker.provinces, Faker.values())],
+        "china",
+        emphasis_label_opts=opts.LabelOpts(is_show=False),
+        emphasis_itemstyle_opts=opts.ItemStyleOpts(border_color="white"),
+    )
+    options = json.loads(c.dump_options())
+    expected = {
+        "label": {"show": False, "position": "top", "margin": 8},
+        "itemStyle": {"borderColor": "white"},
+    }
+    eq_(expected, options["series"][0]["emphasis"])
