723432b4044312a8218e0840eeb040183e3ff04b
陈键冬 <chenjiandongx@qq.com>
2019-06-19 10:29:25 +0800

    V121 (#1205)
    * :hammer: minor refactoring
    
    * Fix: #1197
    
    * Update: v1.2.1

--- install.py
+++ install.py
@@ -5,10 +5,11 @@ about = {}
 with open(os.path.join(here, "pyecharts", "_version.py")) as f:
     exec(f.read(), about)
 
+UNINSTALL = "{} uninstall pyecharts -y"
+INSTALL = "{} install -U dist/pyecharts-{}-py3-none-any.whl --no-cache-dir"
+
 os.system("python setup.py bdist_wheel")
-os.system("pip uninstall pyecharts -y")
-os.system(
-    "pip install -U dist/pyecharts-{}-py3-none-any.whl --no-cache-dir".format(
-        about["__version__"]
-    )
-)
+os.system(UNINSTALL.format("pip"))
+os.system(UNINSTALL.format("pip3"))
+os.system(INSTALL.format("pip", about["__version__"]))
+os.system(INSTALL.format("pip3", about["__version__"]))
