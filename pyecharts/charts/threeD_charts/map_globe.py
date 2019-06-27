9216aa7c7df234f711da58b5ab74e7e7b8d1404d
jaska <wangc_2011@hotmail.com>
2019-06-27 05:47:15 +0100

    3d map (#1213)
    * :sparkles: globe in 3d
    
    * :green_heart: test map3d and map globe
    
    * :lipstick: update import list
    
    * :books: add render examples
    
    * :lipstick: update coding style
    
    * Format: code

---
+++ pyecharts/charts/threeD_charts/map_globe.py
@@ -0,0 +1,49 @@
+import uuid
+
+from jinja2 import Environment
+
+from ... import types
+from ...charts.basic_charts.map import MapMixin
+from ...charts.chart import Chart3D
+from ...commons import utils
+from ...globals import CurrentConfig, NotebookType
+from ...options import InitOpts
+from ...render.display import HTML
+from ...render.engine import RenderEngine
+
+
+class MapGlobe(Chart3D, MapMixin):
+    """
+    Globe Map
+    """
+
+    def __init__(self, init_opts: types.Init = InitOpts()):
+        super().__init__(init_opts)
+
+    def add_schema(self, maptype: str = "china", box_opts: dict = None):
+        self.js_dependencies.add(maptype)
+        return self
+
+    def render(
+        self,
+        path: str = "render.html",
+        template_name: str = "simple_globe.html",
+        env: types.Optional[Environment] = None,
+    ) -> str:
+        return super().render(path=path, template_name=template_name, env=env)
+
+    def render_notebook(self):
+        self.chart_id = uuid.uuid4().hex
+        self._prepare_render()
+        if CurrentConfig.NOTEBOOK_TYPE == NotebookType.JUPYTER_NOTEBOOK:
+            require_config = utils.produce_require_dict(
+                self.js_dependencies, self.js_host
+            )
+            return HTML(
+                RenderEngine().render_chart_to_notebook(
+                    template_name="jupyter_globe.html",
+                    charts=(self,),
+                    config_items=require_config["config_items"],
+                    libraries=require_config["libraries"],
+                )
+            )
