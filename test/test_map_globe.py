198deaf23d38c3fdeca4376b31ee55d160625998
sunhailinLeo <379978424@qq.com>
2019-06-28 10:15:32 +0800

    update test_map_globe code again

--- test/test_map_globe.py
+++ test/test_map_globe.py
@@ -4,6 +4,7 @@ from nose.tools import assert_in, eq_
 
 from example.commons import Faker
 from pyecharts.charts import MapGlobe
+from pyecharts.globals import CurrentConfig, NotebookType
 
 
 @patch("pyecharts.render.engine.write_utf8_html_file")
@@ -20,9 +21,11 @@ def test_map_base(fake_writer):
 
 
 def test_map_globe_in_jupyter():
+    CurrentConfig.NOTEBOOK_TYPE = NotebookType.JUPYTER_NOTEBOOK
+
     c = MapGlobe().add(
         "商家A", [list(z) for z in zip(Faker.provinces, Faker.values())], "china"
     )
-    content = c.render_notebook().data
+    content = c.render_notebook()._repr_html_()
     assert_in("document.createElement('canvas')", content)
     assert_in("baseTexture", content)
