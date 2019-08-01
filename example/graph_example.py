be0c75eee4a95490381d605ea7b726c49446da63
chenjiandongx <chenjiandongx@qq.com>
2019-08-01 23:17:16 +0800

    Update: example

--- example/graph_example.py
+++ example/graph_example.py
@@ -82,24 +82,32 @@ def graph_weibo() -> Graph:
 
 @C.funcs
 def graph_les_miserables():
-    with open(os.path.join("fixtures", "les-miserables-nodes.json"), "r", encoding="utf-8") as f:
-        nodes = json.load(f)
-    with open(os.path.join("fixtures", "les-miserables-links.json"), "r", encoding="utf-8") as f:
-        links = json.load(f)
-    with open(os.path.join("fixtures", "les-miserables-category.json"), "r", encoding="utf-8") as f:
-        category = json.load(f)
+    with open(
+        os.path.join("fixtures", "les-miserables.json"), "r", encoding="utf-8"
+    ) as f:
+        j = json.load(f)
+        nodes = j["nodes"]
+        links = j["links"]
+        categories = j["categories"]
+
     c = (
         Graph(init_opts=opts.InitOpts(width="1000px", height="600px"))
-            .add(
+        .add(
             "",
             nodes=nodes,
             links=links,
-            categories=category,
+            categories=categories,
             layout="circular",
-            # label_opts=opts.LabelOpts(is_show=False),
-            # linestyle_opts=opts.LineStyleOpts(width=0.5, curve=0.3, opacity=0.7),
+            is_rotate_label=True,
+            linestyle_opts=opts.LineStyleOpts(color="source", curve=0.3),
+            label_opts=opts.LabelOpts(position="right"),
         )
-            .set_global_opts(title_opts=opts.TitleOpts(title="Graph-Les Miserables"))
+        .set_global_opts(
+            title_opts=opts.TitleOpts(title="Graph-Les Miserables"),
+            legend_opts=opts.LegendOpts(
+                orient="vertical", pos_left="2%", pos_top="20%"
+            ),
+        )
     )
     return c
 
