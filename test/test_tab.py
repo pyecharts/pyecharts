6aab377ff5840593b8edb2be4ffe023af5dbee12
chenjiandongx <chenjiandongx@qq.com>
2019-08-24 00:07:27 +0800

    Add: tests

--- test/test_tab.py
+++ test/test_tab.py
@@ -1,6 +1,6 @@
 from unittest.mock import patch
 
-from nose.tools import assert_in
+from nose.tools import assert_in, assert_true, eq_
 
 from example.commons import Faker
 from pyecharts.charts import Bar, Line, Tab
@@ -23,3 +23,28 @@ def test_tab_base(fake_writer):
     _, content = fake_writer.call_args[0]
     assert_in("bar-example", content)
     assert_in("line-example", content)
+
+
+def test_tab_render_embed():
+    bar = _create_bar()
+    content = Tab().add(bar, "bar").render_embed()
+    assert_true(len(content) > 1000)
+
+
+def test_page_jshost_default():
+    bar = _create_bar()
+    tab = Tab().add(bar, "bar")
+    eq_(tab.js_host, "https://assets.pyecharts.org/assets/")
+
+
+def test_tab_jshost_custom():
+    from pyecharts.globals import CurrentConfig
+
+    default_host = CurrentConfig.ONLINE_HOST
+    custom_host = "http://localhost:8888/assets/"
+    CurrentConfig.ONLINE_HOST = custom_host
+    bar = _create_bar()
+    line = _create_line()
+    tab = Tab().add(bar, "bar").add(line, "line")
+    eq_(tab.js_host, custom_host)
+    CurrentConfig.ONLINE_HOST = default_host
--- test/test_utils.py
+++ test/test_utils.py
@@ -7,3 +7,15 @@ def test_utils_produce_require_dict():
     cfg = utils.produce_require_dict(utils.OrderedSet("echarts"), "https://example.com")
     eq_(cfg["config_items"], ["'echarts':'https://example.comecharts.min'"])
     eq_(cfg["libraries"], ["'echarts'"])
+
+
+def test_js_code():
+    fn = "function() { console.log('test_js_code') }"
+    js_code = utils.JsCode(fn)
+    eq_(js_code.js_code, "--x_x--0_0--{}--x_x--0_0--".format(fn))
+
+
+def test_ordered_set():
+    s = utils.OrderedSet()
+    s.add("a", "b", "c")
+    eq_(s.items, ["a", "b", "c"])
