64bae74b997c5304bc4c1db346d8a61e2d49f5ba
chenjiandongx <chenjiandongx@qq.com>
2019-08-23 17:24:28 +0800

    Update: example

--- example/pie_example.py
+++ example/pie_example.py
@@ -1,6 +1,7 @@
 from example.commons import Collector, Faker
 from pyecharts import options as opts
 from pyecharts.charts import Page, Pie
+from pyecharts.commons.utils import JsCode
 
 C = Collector()
 
@@ -18,31 +19,46 @@ def pie_base() -> Pie:
 
 @C.funcs
 def pie_multiple_base() -> Pie:
+    fn = """
+    function(params) {
+        if(params.name == '其他')
+            return '\\n\\n\\n' + params.name + ' : ' + params.value + '%';
+        return params.name + ' : ' + params.value + '%';
+    }
+    """
+
+    def new_label_opts():
+        return opts.LabelOpts(formatter=fn, position="center")
+
     c = (
         Pie()
         .add(
             "",
             [list(z) for z in zip(["剧情", "其他"], [25, 75])],
             center=["20%", "30%"],
-            radius=[40, 60],
+            radius=[60, 80],
+            label_opts=new_label_opts(),
         )
         .add(
             "",
             [list(z) for z in zip(["奇幻", "其他"], [24, 76])],
             center=["55%", "30%"],
-            radius=[40, 60],
+            radius=[60, 80],
+            label_opts=new_label_opts(),
         )
         .add(
             "",
             [list(z) for z in zip(["爱情", "其他"], [14, 86])],
             center=["20%", "70%"],
-            radius=[40, 60],
+            radius=[60, 80],
+            label_opts=new_label_opts(),
         )
         .add(
             "",
             [list(z) for z in zip(["惊悚", "其他"], [11, 89])],
             center=["55%", "70%"],
-            radius=[40, 60],
+            radius=[60, 80],
+            label_opts=new_label_opts(),
         )
         .set_global_opts(
             title_opts=opts.TitleOpts(title="Pie-多饼图基本示例"),
@@ -50,7 +66,6 @@ def pie_multiple_base() -> Pie:
                 type_="scroll", pos_top="20%", pos_left="80%", orient="vertical"
             ),
         )
-        .set_series_opts(label_opts=opts.LabelOpts(formatter="{b}: {c}"))
     )
     return c
 
