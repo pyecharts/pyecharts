5fb20464ffcdb34d10c050d25fb8311a8403190a
chenjiandongx <chenjiandongx@qq.com>
2021-11-15 14:54:25 +0800

    Fix: 修复 CI tests

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ setup(
     zip_safe=False,
     include_package_data=True,
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
