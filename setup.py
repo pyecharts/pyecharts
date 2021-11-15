f48924a341be108a2213f1e0a61e30d2caa8ebf5
chenjiandongx <chenjiandongx@qq.com>
2021-11-15 15:03:27 +0800

    Update: 暂时仅支持至 python3.9

--- setup.py
+++ setup.py
@@ -93,7 +93,6 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
         "Topic :: Software Development :: Libraries",
     ],
     cmdclass={"upload": UploadCommand},
