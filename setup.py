f08ef4ac9a4ba7e1f2a5d1189af8334062ab653d
chenjiandongx <chenjiandongx@qq.com>
2021-11-15 14:51:43 +0800

    Fix: 修复 CI tests

--- setup.py
+++ setup.py
@@ -92,6 +92,8 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Software Development :: Libraries",
     ],
     cmdclass={"upload": UploadCommand},
