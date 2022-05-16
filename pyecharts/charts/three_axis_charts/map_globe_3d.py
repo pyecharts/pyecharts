1174d79c4a0a7447348f75b39bb1adb02e3441a3
Wapiti <tzrzhuoran@gmail.com>
2022-05-16 09:44:33 +0100

    Update map_globe_3d.py

--- pyecharts/charts/three_axis_charts/map_globe_3d.py
+++ pyecharts/charts/three_axis_charts/map_globe_3d.py
@@ -1,8 +1,13 @@
-from pyecharts.charts import MapGlobe, Map3D, Geo
-from pyecharts import types
-from pyecharts.options import InitOpts
+from pyecharts.charts import MapGlobe, Geo
+
+from ... import options as opts
+from ... import types
+from ...options import InitOpts
 
 class  MapGlobe3D(Geo, MapGlobe):
+    '''
+        Globe 3D Map 
+    '''
     def __init__(self, init_opts: types.Init = InitOpts()):
         super().__init__(init_opts)
         self._3d_chart_type = "MapGlobe3D"
