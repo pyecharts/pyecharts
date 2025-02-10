d4ef2d73f83070b4fd4e5089d8de31073e5ac4ad
GokoRuri <qq@yjc.anonaddy.me>
2025-02-10 19:19:35 +0800

    Update test_grid.py
    控制单行长度<89

--- test/test_grid.py
+++ test/test_grid.py
@@ -13,7 +13,8 @@ class TestGridComponent(unittest.TestCase):
             chart.options.get(key), list
         ) and all(
             isinstance(
-                item, (opts.DataZoomOpts if key == "dataZoom" else opts.VisualMapOpts, dict)
+                item, 
+                (opts.DataZoomOpts if key == "dataZoom" else opts.VisualMapOpts, dict)
             ) for item in chart.options.get(key)
         )
 
