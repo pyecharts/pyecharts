122269e07177390f51c942c4a68014052ebf86cb
LeoSun <379978424@qq.com>
2025-04-29 13:12:24 +0800

    [hotfix]fix page missing css_libs

--- pyecharts/render/engine.py
+++ pyecharts/render/engine.py
@@ -58,7 +58,7 @@ class RenderEngine:
                             links.append(_link)
                         break
         chart.dependencies = links
-        chart.css_libs = css_links
+        chart.css_libs = chart.css_libs + css_links
         return chart
 
     def render_chart_to_file(self, template_name: str, chart: Any, path: str, **kwargs):
