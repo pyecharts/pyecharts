7a713f61e77bf9d9f7cfe0b836d228d2d8dff014
LeoSun <379978424@qq.com>
2025-10-09 10:28:55 +0800

    update line.py and update workflows

--- setup.py
+++ setup.py
@@ -97,6 +97,7 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
         "Topic :: Software Development :: Libraries",
     ],
     cmdclass={"upload": UploadCommand},
